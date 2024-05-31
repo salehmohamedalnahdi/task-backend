34dfa31e777dc2a96ee8da8f1ba3e199b954a2e1
salehmohamedalnahdi <smbt2011@gmail.com>
2024-05-31 19:16:38 +0300

    hjlw

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const routes=require('./routes/route.js')
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
